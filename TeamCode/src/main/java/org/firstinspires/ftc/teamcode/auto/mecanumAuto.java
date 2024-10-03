1257f512339005787d3d8befa643bafaa4635f75
cnetz <Cameronjnetz@gmail.com>
2024-10-03 14:23:11 -0700

    big

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/mecanumAuto.java
@@ -0,0 +1,320 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.teleOp.twoWheelDrive;
+
+@Autonomous
+public class mecanumAuto extends OpMode {
+    private PIDController controller;
+    double p = 0.004,i = 0, d = 0.0002;
+    double f = 0.03;
+    int armTarget = 0;
+    int armThreshold = 10;
+    double ticksInDegree = 285 / 180;//1425
+    double cpr = 537.7;
+    double gearRatio = 1;
+    double diameter = 3.77;
+    double slideDiameter = 1.5;
+    double cpi = (cpr * gearRatio)/(Math.PI * diameter);
+    double cpiSlide = (cpr * gearRatio)/(Math.PI * slideDiameter);
+    double bias = 1.0;
+    double conversion = cpi * bias;
+    double robotWidth = 12.9;
+    double turnCF = Math.PI * robotWidth;
+    private DcMotor jointMotor; // location
+    private DcMotor frontLeftMotor; // location
+    private DcMotor backLeftMotor; // location
+    private DcMotor backRightMotor; // location
+    private DcMotor frontRightMotor; // location
+    private DcMotor slideMotor; // location
+    private Servo wristServo;
+    private Servo clawServo;
+    private Servo basketServo;
+    private ElapsedTime newTimer = new ElapsedTime();
+    IMU imu;
+    private boolean fiveSeconds = false;
+    private boolean exit = false;
+    private enum OrderState{
+        FIRST,SECOND,THIRD
+    }
+    private OrderState currentOrderState = OrderState.FIRST;
+    private enum SlideState{
+        IDLE,MOVING,COMPLETED
+    }
+    private SlideState currentSlideState = SlideState.IDLE;
+    private enum DriveState{
+        IDLE,MOVING,COMPLETED
+    }
+    private DriveState currentDriveState = DriveState.IDLE;
+    private enum armState{
+        IDLE, HOLDING,MOVING//same as completed
+    }
+    private armState currentArmState = armState.IDLE;
+    @Override
+    public void init() {
+        controller = new PIDController(p,i,d);
+
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        jointMotor = hardwareMap.get(DcMotorEx.class, "jointMotor");
+        slideMotor = hardwareMap.get(DcMotor.class, "slideMotor");
+        wristServo = hardwareMap.get(Servo.class,"wristServo");
+        clawServo = hardwareMap.get(Servo.class,"clawServo");
+        basketServo = hardwareMap.get(Servo.class,"basketServo");
+        imu = hardwareMap.get(IMU.class,"imu");
+
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        jointMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //jointMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        // Now initialize the IMU with this mounting orientation
+        // Note: if you choose two conflicting directions, this initialization will cause a code exception.
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+    }
+
+    public void init_loop() {
+        if (gamepad1.y) {
+            telemetry.addData("Yaw", "Resetting\n");
+            imu.resetYaw();
+        } else {
+            telemetry.addData("Yaw", "Press Y (triangle) on Gamepad to reset\n");
+        }
+        updateTelemetry();
+    }
+
+    @Override
+    public void start() {
+        clawServo.setPosition(0.75);
+        newTimer.reset();
+    }
+
+    @Override
+    public void loop() {
+        updateTelemetry();
+        switch(currentOrderState){
+            case FIRST:
+                if (currentArmState == armState.IDLE){
+                    setTargetArm(3000);
+                    wristServo.setPosition(0.55);
+                }
+                if (currentArmState == armState.HOLDING){
+                    currentArmState = armState.IDLE;
+                    currentOrderState = OrderState.SECOND;
+                }
+                /*if (currentDriveState == DriveState.IDLE && currentSlideState == SlideState.IDLE){
+                //moveSlide(12,0.2);
+                // moveToPos(20,0.2);
+            }
+                if (currentDriveState == DriveState.COMPLETED && currentSlideState == SlideState.COMPLETED){
+                    currentDriveState = DriveState.IDLE;
+                    currentSlideState = SlideState.IDLE;
+                    currentOrderState = OrderState.SECOND;
+                }*/
+                break;
+            case SECOND:
+                if (currentDriveState == DriveState.IDLE && currentSlideState == SlideState.IDLE){
+                    // moveSlide(-12,0.2);
+                    // moveToPos(-20,0.2);
+                }
+                if (currentDriveState == DriveState.COMPLETED && currentSlideState == SlideState.COMPLETED){
+                    currentDriveState = DriveState.IDLE;
+                    currentSlideState = SlideState.IDLE;
+                    currentOrderState = OrderState.THIRD;
+                }
+                break;
+            case THIRD:
+                break;
+        }
+
+        driveFSM();
+        armFSM();
+        slideFSM();
+    }
+    @Override
+    public void stop() {
+        exit = true;
+    }
+
+    public void updateTelemetry(){
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+
+        telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
+        telemetry.addData("time", newTimer.seconds());
+        telemetry.addData("Slide", slideMotor.getCurrentPosition());
+        telemetry.addData("frontLeft", frontLeftMotor.getCurrentPosition());
+        telemetry.addData("frontRight", frontRightMotor.getCurrentPosition());
+        telemetry.addData("backLeft", backLeftMotor.getCurrentPosition());
+        telemetry.addData("backRight", backRightMotor.getCurrentPosition());
+        telemetry.addData("DriveState", currentDriveState);
+        telemetry.addData("SlideState", currentSlideState);
+        telemetry.addData("ArmState", currentArmState);
+        telemetry.addData("armTarget", armTarget);
+        telemetry.addData("jointPos", jointMotor.getCurrentPosition());
+        telemetry.addData("jointPower", jointMotor.getPower());
+        telemetry.update();
+    }
+
+    public void moveToPos(double inches, double speed) {
+        int move = (int)(Math.round(inches * conversion));
+
+        frontLeftMotor.setTargetPosition(frontLeftMotor.getCurrentPosition() + move);
+        frontRightMotor.setTargetPosition(frontRightMotor.getCurrentPosition() + move);
+        backLeftMotor.setTargetPosition(backLeftMotor.getCurrentPosition() + move);
+        backRightMotor.setTargetPosition(backRightMotor.getCurrentPosition() + move);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeftMotor.setPower(speed);
+        frontRightMotor.setPower(speed);
+        backLeftMotor.setPower(speed);
+        backRightMotor.setPower(speed);
+
+        currentDriveState = DriveState.MOVING;
+
+    }
+    public void moveSlide(double inches, double speed){
+        int move = (int)(Math.round(inches * cpiSlide));
+        slideMotor.setTargetPosition(slideMotor.getCurrentPosition() + move);
+
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        slideMotor.setPower(speed);
+        currentSlideState = SlideState.MOVING;
+
+    }
+    public void moveJoint(double inches, double speed) {
+        int move = (int) (Math.round(inches * cpiSlide * 3));
+        jointMotor.setTargetPosition(move);
+
+        jointMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        jointMotor.setPower(speed);
+    }
+    public void setTargetArm(int target){
+        armTarget = target;
+        currentArmState = armState.MOVING;
+    }
+    public void moveArm(int target){
+        controller.setPID(p,i,d);
+        int jointPos = jointMotor.getCurrentPosition();
+        double pid = controller.calculate(jointPos,target);
+        double ff = Math.cos(Math.toRadians(target / ticksInDegree)) * f;
+        double power = pid + ff;
+        jointMotor.setPower(power);
+    }
+    public void armFSM(){
+        switch (currentArmState){
+            case IDLE:
+                int currentPos1 = jointMotor.getCurrentPosition();
+                moveArm(currentPos1);
+                break;
+            case HOLDING:
+                moveArm(armTarget);
+                break;
+            case MOVING:
+                moveArm(armTarget);
+                double currentPos2 = jointMotor.getCurrentPosition();
+
+                if (Math.abs((armTarget - currentPos2)) < armThreshold){
+                    currentArmState = armState.HOLDING;
+                }
+
+        }
+    }
+    public void slideFSM(){
+        switch (currentSlideState) {
+            case IDLE:
+                break;
+
+            case MOVING:
+                // Check if the slide has reached the target
+                if (!slideMotor.isBusy()) {
+                    // If it's done moving, transition to COMPLETED state
+                    slideMotor.setPower(0);  // Stop the motor
+                    currentSlideState = SlideState.COMPLETED;
+                    break;
+                }
+                break;
+
+            case COMPLETED:
+                // Now transition back to IDLE for future movement
+                break;
+
+        }
+
+    }
+    public void driveFSM(){
+        switch (currentDriveState) {
+            case IDLE:
+                break;
+
+            case MOVING:
+                // converts inches to cpr for driving forward and back
+                // Check if the slide has reached the target
+                if (!frontLeftMotor.isBusy() && !frontRightMotor.isBusy() && !backLeftMotor.isBusy() && !backRightMotor.isBusy()) {
+                    // If it's done moving, transition to COMPLETED state
+                    frontLeftMotor.setPower(0);
+                    frontRightMotor.setPower(0);
+                    backLeftMotor.setPower(0);
+                    backRightMotor.setPower(0);
+
+                    currentDriveState = DriveState.COMPLETED;
+                    break;
+                }
+                break;
+
+            case COMPLETED:
+                // Now transition back to IDLE for future movement
+                break;
+        }
+
+    }
+    //private double calculateSinusoidalSpeed(int currentDistance, int totalDistance)
+    //fractionTraveled = (double) currentDistance / totalDistance;
+    //speed = baseSpeed + (maxSpeed - baseSpeed) * Math.sin(Math.PI * fractionTraveled);
+    //return speed;
+
+    //private double applyAccelerationControl(double currentSpeed, double targetSpeed, double maxAcceleration)
+    // speedDifference = targetSpeed - currentSpeed
+    // if(speedDifference > maxAcceleration) {
+    // currentSpeed += maxAcceleration
+    // } else if (speedDifference < -maxAcceleration) {
+    // currentSpeed -= maxAcceleration
+    // } else {
+    // currentSpeed = targetSpeed;
+    // }
+    // return currentSpeed;
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -0,0 +1,216 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.teleOp.twoWheelDrive;
+
+@TeleOp
+public class mecanumDrive extends LinearOpMode {
+    private DcMotor jointMotor; // location
+    private DcMotor frontLeftMotor; // location
+    private DcMotor backLeftMotor; // location
+    private DcMotor backRightMotor; // location
+    private DcMotor frontRightMotor; // location
+    private DcMotor slideMotor; // location
+    private Servo wristServo; // location
+    private Servo clawServo; // location
+    private Servo basketServo; // location
+
+    private ElapsedTime newTimer = new ElapsedTime();
+    private enum SlideState {
+        COMPLETED,
+        IDLE,
+        MANUAL,
+        MOVING
+    }
+    private SlideState currentSlideState = SlideState.IDLE;
+
+    @Override
+    public void runOpMode()  {
+
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        jointMotor = hardwareMap.get(DcMotor.class, "jointMotor");
+        slideMotor = hardwareMap.get(DcMotor.class, "slideMotor");
+        wristServo = hardwareMap.get(Servo.class,"wristServo");
+        clawServo = hardwareMap.get(Servo.class,"clawServo");
+        basketServo = hardwareMap.get(Servo.class,"basketServo");
+
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        jointMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //Reverse the other motors and sex X to not negative
+
+        double cpr = 537.7;
+        double gearRatio = 1;
+        double slideDiameter = 1.5;
+        double cpiSlide = (cpr * gearRatio)/(Math.PI * slideDiameter);
+        double bias = 1.1;
+
+        double changeSpeed = 0.65;
+        boolean changeSpeedPos = false;
+        boolean wristPos = false;
+        boolean clawPos = false;
+        boolean wristPosX = false;
+        boolean rbPos = false;
+
+        waitForStart();
+        //wristServo.setPosition(0.45);
+
+        newTimer.reset();
+
+        ButtonHandler buttonHandler = new ButtonHandler();
+
+        if (isStopRequested()) return;
+
+        while(opModeIsActive()) {
+
+            double y = -gamepad1.left_stick_y;// set y to gamepad 1 left stick y
+            double x = gamepad1.left_stick_x * bias;// set x to gamepad 1 left stick x
+            double rx = gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);//maxumum x+y =1
+            double backRightPower = ((y + x - rx) / denominator);
+            double frontLeftPower = ((y + x + rx) / denominator);// front left motor power = y+x/den
+            double frontRightPower = ((y - x - rx) / denominator);
+            double backLeftPower = ((y - x + rx) / denominator);// front left motor power = y+x/den
+
+            frontRightMotor.setPower(frontRightPower * changeSpeed);//back right motor it sets power/change speed
+            backRightMotor.setPower(backRightPower * changeSpeed);//back right motor it sets power/change speed
+            frontLeftMotor.setPower(frontLeftPower * changeSpeed);//back right motor it sets power/change speed
+            backLeftMotor.setPower(backLeftPower * changeSpeed);//for back left  motor it  sets power/change speed
+
+            //slideMotor.setPower(gamepad2.left_stick_y)
+            if (gamepad2.right_trigger >0){
+                jointMotor.setPower(gamepad2.right_trigger);
+            } else if(gamepad2.left_trigger >0) {
+                jointMotor.setPower(-gamepad2.left_trigger);
+            }else{
+                jointMotor.setPower(0);
+            }
+
+            switch (currentSlideState) {
+                case IDLE:
+                    if(gamepad2.left_stick_y != 0){
+                        currentSlideState = SlideState.MANUAL;
+                    } else if(gamepad2.dpad_down) {
+                        int move = (int)(Math.round(-20 * cpiSlide));
+                        slideMotor.setTargetPosition(slideMotor.getCurrentPosition() + move);
+
+                        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                        slideMotor.setPower(0.2);
+
+                        currentSlideState = SlideState.MOVING;
+                    }
+                    break;
+
+                case MOVING:
+                    // Check if the slide has reached the target
+                    if (!slideMotor.isBusy()) {
+                        // If it's done moving, transition to COMPLETED state
+                        currentSlideState = SlideState.COMPLETED;
+                    }
+                    break;
+
+                case COMPLETED:
+                    // Stop the motor and set back to RUN_USING_ENCODER for manual control
+                    slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    slideMotor.setPower(0);  // Stop the motor
+
+                    // Now transition back to IDLE for future movement
+                    currentSlideState = SlideState.IDLE;
+                    break;
+
+                case MANUAL:
+                    slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    slideMotor.setPower(gamepad2.left_stick_y);
+                    // If no manual control is happening, return to IDLE
+                    if (Math.abs(gamepad2.left_stick_y) == 0) {
+                        currentSlideState = SlideState.IDLE;
+                    }
+                    break;
+            }
+
+            if (buttonHandler.isPressedOnceA_1(gamepad1.a)) {
+                if (changeSpeedPos) {
+                    changeSpeedPos = false;
+                    changeSpeed = 0.65; //speed is 1
+
+                } else { //speed is halved
+                    changeSpeed = 0.25 ;
+                    changeSpeedPos = true;
+                }
+            }
+            if (buttonHandler.isPressedOnceB_2(gamepad2.b)) {
+                if (wristPos) {
+                    wristServo.setPosition(0.45);
+                    wristPos = false;
+
+                } else {
+                    wristPos = true;
+                    wristServo.setPosition(0.75);
+                }
+            }
+            if (buttonHandler.isPressedOnceA_2(gamepad2.a)) {
+                if (clawPos) {
+                    clawServo.setPosition(.1);
+                    clawPos = false;
+
+                } else {
+                    clawPos = true;
+                    clawServo.setPosition(0.75);
+                }
+            }
+            if (buttonHandler.isPressedOnceX_2(gamepad2.x)) {
+                if (wristPosX) {
+                    wristServo.setPosition(0.45);
+                    wristPosX = false;
+
+                } else {
+                    wristPosX = true;
+                    wristServo.setPosition(0.1);
+                }
+            }
+            if (gamepad2.y) {
+                clawServo.setPosition(0.6);
+            }  else {
+                clawServo.setPosition(0.75); //if claw loose adjust + 0.05
+            }
+            if (buttonHandler.isPressedOnceRB_2(gamepad2.right_bumper)) {
+                if (rbPos) {
+                    basketServo.setPosition(0.7);
+                    rbPos = false;
+
+                } else {
+                    rbPos = true;
+                    basketServo.setPosition(0.3);
+                }
+            }
+
+
+            telemetry.addData("Slide State", currentSlideState);
+            telemetry.addData("time", newTimer.seconds());// tells us the time from when we press start
+            telemetry.addData("jointMotorPos", jointMotor.getCurrentPosition());
+            telemetry.addData("slideMotorPos", slideMotor.getCurrentPosition());
+            telemetry.addData("wristServo", wristServo.getPosition());
+            telemetry.update();
+        }
+    }
+}
+
+
+
